9e9086314626091d5b58d44391cb4d856ea65138
jian <2110179446@qq.com>
2023-04-06 19:48:51 +0800

    服务器图片接口

--- src/main/java/com/example/mh10/Mh10Application.java
+++ src/main/java/com/example/mh10/Mh10Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Mh10Application {
-//A
+
     public static void main(String[] args) {
         SpringApplication.run(Mh10Application.class, args);
     }
